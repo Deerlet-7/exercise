265aff4b427aed65bc2f158a5f7014b8059c3779
Deerlet-7 <liyan000611@outlook.com>
2019-10-14 23:01:42 +0800

    1/n（n从1递增到100）相加

--- ex 191013/Test4.java
+++ ex 191013/Test4.java
@@ -1,5 +1,5 @@
 public class Test4{
-	//计算1/1-1/2+1/3-1/4+1/5 …… + 1/99 - 1/100 的值
+	//计算1/1-1/2+1/3-1/4+1/5 …… + 1/99 - 1/100 值
 	public static void main(String[] args){
 		int a = 1;
 		int flg = 1;
