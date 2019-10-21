578fcdb837116403fa1ba05cf26e30811d18cc79
Deerlet-7 <liyan000611@outlook.com>
2019-10-21 23:22:37 +0800

    空指针异常实例

---
+++ ex 191021/src/TestDemo1.java
@@ -0,0 +1,8 @@
+public class TestDemo1 {
+    public static void main(String[] args) {
+//        int[] array = null;
+//        System.out.println(array.length);// java.lang.NullPointerException
+        String str = null;
+        System.out.println(str.length());//java.lang.NullPointerException空指针异常
+    }
+}
