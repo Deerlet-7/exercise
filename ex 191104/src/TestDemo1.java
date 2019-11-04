8bc4663e5f153bf097c609756f39ab88c5297e61
Deerlet-7 <liyan000611@outlook.com>
2019-11-04 23:25:50 +0800

    比赛练习

--- ex 191104/src/TestDemo1.java
+++ ex 191104/src/TestDemo1.java
@@ -1,6 +1,30 @@
 import java.util.Scanner;
 
 public class TestDemo1 {
+	   public static int fibonacci2(int n){
+        int a = 1;
+        int b = 1;
+        int c = 1;//若n=1或2时直接retyrn1.
+        for (int i = 3; i <= n; i++) {//i<=n,否则n=3时依然无法进入循环
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+    public static int fibonacci(int n){
+        if(n == 1 || n == 2){
+            return 1;
+        }else {
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("输入要查看斐波那契数列的位数：");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(TestDemo1.fibonacci2(n));
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("输入要判断的年份");
