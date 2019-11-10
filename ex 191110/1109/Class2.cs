14ac4cb9c50544e3b39f6409b960cbf0442fd8ac
Deerlet-7 <liyan000611@outlook.com>
2019-11-10 14:13:26 +0800

    c# 求PAI的值 求圆面积

---
+++ ex 191110/1109/Class2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1109
+{
+    class Class2
+    {
+        static void Main(string[] args)
+        {
+            //求Π
+            double sum = 0.5, t, t1, t2, t3, p = 0.5 * 0.5;
+            int odd = 1, even = 2;
+            t = t1 = t2 = 1.0; t3 = 0.5;
+            while(t>1e-10)
+            {
+                t1 = t1 * odd / even;
+                odd += 2; even += 2;
+                t2 = 1.0 / odd;
+                t3 = t3 * p;
+                t = t1 * t2 * t3;
+                sum += t;
+            }
+            Console.WriteLine("PAI为{0}", sum);
+            Console.Write("输入所求半径");
+            string input = Console.ReadLine();
+            int r = Convert.ToInt32(input);
+            double PAI = sum;
+            double squ = PAI * r * r;
+            Console.WriteLine("面积为{0}", squ);
+        }
+    }
+}
