ae8f9f92c951e943ec3c355fe1bdb2f57f1c8404
Deerlet-7 <liyan000611@outlook.com>
2019-11-10 15:50:56 +0800

    求圆面积

--- ex 191110/1109/Class2.cs
+++ ex 191110/1109/Class2.cs
@@ -10,24 +10,23 @@ namespace _1109
     {
         static void Main(string[] args)
         {
-            //求Π
-            double sum = 0.5, t, t1, t2, t3, p = 0.5 * 0.5;
+             double sum = 0.5, t, t1, t2, t3, p = 0.5 * 0.5;
             int odd = 1, even = 2;
-            t = t1 = t2 = 1.0; t3 = 0.5;
-            while(t>1e-10)
+            t = t1 = t2 = 1.0;t3 = 0.5;
+            while (t > 1e-10)
             {
                 t1 = t1 * odd / even;
-                odd += 2; even += 2;
+                odd += 2;even += 2;
                 t2 = 1.0 / odd;
-                t3 = t3 * p;
-                t = t1 * t2 * t3;
+                t3 = t3*p;
+                t = t1*t2*t3;
                 sum += t;
             }
-            Console.WriteLine("PAI为{0}", sum);
+            Console.WriteLine("\nPI={0,10:f8}", sum * 6);
             Console.Write("输入所求半径");
-            string input = Console.ReadLine();
-            int r = Convert.ToInt32(input);
-            double PAI = sum;
+            string input = Console.ReadLine();//readline()读入的为字符串形式
+            int r = Convert.ToInt32(input);//对input进行强转
+            double PAI = sum * 6;
             double squ = PAI * r * r;
             Console.WriteLine("面积为{0}", squ);
         }
