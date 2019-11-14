1a65725d944924a38843d5c6fb1e998774770342
Deerlet-7 <liyan000611@outlook.com>
2019-11-14 15:21:37 +0800

    c#练习2

---
+++ ex 191114/ex 191114/Test 2-5.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_191114
+{
+    class Test_2_5
+    {
+
+    }
+}
