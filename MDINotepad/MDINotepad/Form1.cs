0a0726bbb07e641a5fab1e42f60bcb4a5d2afc74
Deerlet-7 <liyan000611@outlook.com>
2019-11-29 22:35:18 +0800

    c#课本练习

---
+++ MDINotepad/MDINotepad/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDINotepad
+{
+    public partial class MDIForm : Form
+    {
+        public MDIForm()
+        {
+            InitializeComponent();
+        }
+
+        private void 窗口WToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WinForm/WinForm/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void 新建NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm MDIChild = new ChildForm();
+            MDIChild.MdiParent = this;
+            MDIChild.Show();
+        }
+
+        private void 窗口ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form activeChild = this.ActiveMdiChild;
+            if(activeChild != null)
+            {
+                try
+                {
+                    RichTextBox theBox = (RichTextBox)activeChild.ActiveControl;
+                    if(theBox != null)
+                    {
+                        Clipboard.SetDataObject(theBox.SelectedText);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("you need to select a RichTextBox");
+                }
+            }
+        }
+    }
+}
