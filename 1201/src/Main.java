faf49321b6e6e482b0e7f90355aa23c2d9098d50
Deerlet-7 <liyan000611@outlook.com>
2019-12-01 17:44:54 +0800

    字符串先分割 再拼接

---
+++ 1201/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public static String func(String str){
+        String[] s = str.split(" ");
+        StringBuilder ret = new StringBuilder("");
+        for (String ss:s){
+            ret.append(ss);
+        }
+        return ret.toString();
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        String ret = func(str);
+        System.out.println(ret);
+    }
+}
