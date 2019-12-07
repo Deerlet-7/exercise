e15a85a1c806ee2425cfdfb46078f8b0d3c2b3b0
Deerlet-7 <liyan000611@outlook.com>
2019-12-07 22:33:40 +0800

    daily ex

---
+++ ex 1206/src/Frog.java
@@ -0,0 +1,14 @@
+class Frog extends Animal implements IRunning, ISwimming {
+    public Frog(String name) {
+        super(name);
+    }
+    @Override
+    public void run() {
+        System.out.println(this.name + "正在往前跳");
+    }
+    @Override
+    public void swim() {
+        System.out.println(this.name + "正在蹬腿游泳");
+    }
+}
+
--- ex 191115/src/TestDemo1.java
+++ ex 191115/src/TestDemo1.java
@@ -1,10 +1,10 @@
 public class TestDemo1 {
-    public static void main1(String[] args) {
+    public static void main(String[] args) {
         Animal animal= new Bird("niao ",3,"black");
         animal.eat();
-//        Bird bird = (Bird)animal;//animale无法直接调用fly(),只能将其强转为Bird类型来调用fly()方法
-//        bird.fly();
-        Cat cat = (Cat)animal;
-        cat.jump();
+        Bird bird = (Bird)animal;//animale无法直接调用fly(),只能将其强转为Bird类型来调用fly()方法
+        bird.fly();
+//        Cat cat = (Cat)animal;
+//        cat.jump();
     }
 }
