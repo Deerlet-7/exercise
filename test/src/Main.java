926aec32b6ee4657086cc5fa8abcb2860b3a1d9e
Deerlet-7 <liyan000611@outlook.com>
2019-12-07 22:34:20 +0800

    daily ex

---
+++ test/src/Main.java
@@ -0,0 +1,76 @@
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+class MySignalList {
+    public ListNode head;
+
+    public MySignalList() {
+        this.head = null;
+    }
+    public void addLast(int data){
+        ListNode node = new ListNode(data);
+        ListNode cur =this.head ;
+        if(this.head == null) {
+            this.head = node;
+        }else {
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = node;
+        }
+    }
+    public MySignalList Merge(MySignalList list1,MySignalList list2) {
+        if(list1==null){
+            return list2;
+        }
+        if(list2==null){
+            return list1;
+        }
+        ListNode cur = list1.head;
+        ListNode cur2 = list2.head;
+        MySignalList list = null;
+        if(cur.val<cur2.val){
+            list = cur;
+            cur = cur.next;
+        }else{
+            list = cur2;
+            cur2 = cur2.next;
+        }
+        while(cur!=null||cur2!=null){
+            if(cur.val<cur2.val){
+                list.next = cur;
+                cur = cur.next;
+            }else{
+                list.next = cur2;
+                cur2 = cur2.next;
+            }
+        }
+        if(cur == null){
+            while(cur2!=null){
+                list.next = cur2;
+                cur2=cur2.next;
+            }
+
+        }else{
+            while(cur !=null){
+                list.next = cur;
+                cur = cur.next;
+            }
+
+        }
+        return list;
+    }
+    public void display() {
+        ListNode cur = this.head;
+        while (cur != null) {
+            System.out.print(cur.val+" ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+}
---
+++ test/src/Test.java
@@ -0,0 +1,23 @@
+public class Test {
+    public static void main(String[] args) {
+        MySignalList mySignalList = new MySignalList();
+        mySignalList.addLast(1);
+        mySignalList.addLast(3);
+        mySignalList.addLast(5);
+        mySignalList.addLast(7);
+        mySignalList.addLast(9);
+        mySignalList.display();
+        MySignalList mySignalList2 = new MySignalList();
+        mySignalList2.addLast(1);
+        mySignalList2.addLast(2);
+        mySignalList2.addLast(3);
+        mySignalList2.addLast(4);
+        mySignalList2.addLast(5);
+        mySignalList2.display();
+        MySignalList mySignalList3 = new MySignalList();
+        mySignalList3.Merge(mySignalList,mySignalList2);
+
+    }
+
+
+}
