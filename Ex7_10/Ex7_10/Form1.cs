212b72fc33a1663aa1cf20e7ed066030f1b455b0
Deerlet-7 <liyan000611@outlook.com>
2019-12-10 17:58:17 +0800

    c#

---
+++ Ex7_10/Ex7_10/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex7_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Bitmap myBitmap = new Bitmap("C:\\boy.gif");
+            Graphics g = pictureBox1.CreateGraphics();
+            g.DrawImage(myBitmap, 0, 0);
+
+        }
+    }
+}
