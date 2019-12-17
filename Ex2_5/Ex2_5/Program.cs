534f657d2c18454e3c43a797b842aba6ec0261e6
Deerlet-7 <liyan000611@outlook.com>
2019-12-17 23:01:49 +0800

    c#习题

---
+++ Ex2_5/Ex2_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double PAI = 1;
+            for (double i = 1;i<1001;i++)
+            {
+                PAI *= ((2 * i) / (2 * i - 1)) * ((2 * i) / (2 * i + 1));
+            }
+            Console.Write("{0}", PAI*2);
+            Console.Read();
+        }
+    }
+}
