c534783bff22d255a6828bde53dace57fd94ffd3
Deerlet-7 <liyan000611@outlook.com>
2019-12-17 23:02:26 +0800

    c#习题

---
+++ Ex2_6/Ex2_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int n = Convert.ToInt32(s);
+            Console.WriteLine("十六进制表示: " + Convert.ToString(n,16));
+            Console.Read();
+        }
+    }
+}
