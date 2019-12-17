c6e3ae2edcf44202c6f7fb6bbfd2fd9f1ac985d9
Deerlet-7 <liyan000611@outlook.com>
2019-12-17 23:00:16 +0800

    c#习题

---
+++ Ex2_2/Ex2_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int n = Convert.ToInt32(s);
+            int y = fun(n);
+            Console.WriteLine("n={0} y={1}", n,y);
+        }
+        static int fun(int n)
+        {
+            int ret = 1;
+            int sum = 0;
+            for (int i = n;i>0;i--)
+            {
+                int j = i;
+                ret = 1;
+                while (j!=1)
+                {
+                    ret = ret * j;
+                    j--;
+                }
+                sum += ret;
+            }
+            return sum;
+        }
+    }
+}
