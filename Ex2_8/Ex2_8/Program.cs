749949c2f1e8c9eb7336d2ccea24575fd2e865d7
Deerlet-7 <liyan000611@outlook.com>
2019-12-17 23:17:06 +0800

    c#习题

---
+++ Ex2_8/Ex2_8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 6, 8, 3, 5, 24, 5, 65, 33 };
+            judge(a);
+        }
+        static void judge(int[] a)
+        {
+            int max = 0;
+            int index = 0;
+            for(int i =0; i < a.Length-1; i++)
+            {
+                if (a[i] > a[i + 1])
+                {
+                    max = a[i];
+                    index = i;
+                }
+            }
+            Console.WriteLine("max:{0},index:{1}", max, index);
+        }
+    }
+}
