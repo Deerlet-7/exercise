6139c5ec38f6448e13f4aa066a2f09e687e7f86c
Deerlet-7 <liyan000611@outlook.com>
2019-12-18 17:59:38 +0800

    c#习题

---
+++ Ex2_11/Ex2_11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[4, 4] {{ 1, 2, 3, 4 },{ 2, 3, 4, 5 },{ 3, 4, 5, 6 },{ 4, 5, 6, 7 }};
+            int sum = 0;
+            int j = 3;
+            for (int i = 0;i<4;i++)
+            {
+                while(j>=0)
+                {
+                    int a = arr[i, i];
+                    int b = arr[i, j];
+                    sum += a + b;
+                    j--;
+                    break;
+                }
+            }
+            Console.WriteLine("{0}", sum);
+            Console.Read();
+        }
+    }
+}
