2695f522dff70ed9dd4f76fea06ffda574c9f4bd
Deerlet-7 <liyan000611@outlook.com>
2019-12-18 17:58:38 +0800

    c#习题

---
+++ Ex2_9/Ex2_9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int left = 0;
+            int right = s.Length - 1;
+            while (left < right)
+            {
+                if (s[left] == s[right])
+                {
+                    left += 1;
+                    right -= 1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (left >= right)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
