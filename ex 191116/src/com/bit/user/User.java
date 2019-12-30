9dbfec795b0d5f7b3f4b5b1e32f758e2ba772964
Deerlet-7 <liyan000611@outlook.com>
2019-12-30 22:52:58 +0800

    图书管理系统

--- ex 191116/src/com/bit/user/User.java
+++ ex 191116/src/com/bit/user/User.java
@@ -7,7 +7,7 @@ public abstract class User {
     protected String name;
     protected IOperation[] operations;
     public abstract int menu();
-    //根据用户选项执行一个操作
+    //根据用户选项执行一个操作。
     public void doOperation(int chioce, BookList bookList){
         operations[chioce].work(bookList);
     }
