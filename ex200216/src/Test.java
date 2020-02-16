346575636a8958c85f981617582e2be1228c9244
Deerlet-7 <liyan000611@outlook.com>
2020-02-16 23:24:46 +0800

    数组元素右移

--- ex200216/src/Test.java
+++ ex200216/src/Test.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-//给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+//给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数
 public class Test {
     public static void main(String[] args) {
         int[] nums = new int[]{1,2,3,4,5,6,7};
