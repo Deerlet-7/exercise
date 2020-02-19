1936177210724d2ebe8842859e3cc9694329fe81
Deerlet-7 <liyan000611@outlook.com>
2020-02-20 00:16:37 +0800

    寒假练习

--- winter2020/src/Test.java
+++ winter2020/src/Test.java
@@ -1,21 +1,23 @@
 public class Test {
     public static void main(String[] args) {
-        String ransomNote = "aa";
-        String magazine = "ab";
+        String ransomNote = "bg";
+        String magazine = "efjbdfbbgbjagbddfgdiaigdadhcfcj";
         System.out.println(Test.canConstruct(ransomNote,magazine));
     }
     public static boolean canConstruct(String ransomNote, String magazine) {
         char[] ch = ransomNote.toCharArray();
         char[] ch2 = magazine.toCharArray();
         int k = 0;
-        int j = 0;
+
         for (int i = 0; i < ch.length; i++) {
-            for (; j <ch2.length ; j++) {
+            for (int j = 0; j <ch2.length ;j++ ) {
                 if(ch2[j]==ch[i]){
                     k+=1;
+                    i++;
                     continue;
                 }else {
-                    break;
+                    k=0;
+                    continue;
                 }
             }
         }
