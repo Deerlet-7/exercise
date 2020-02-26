65e4190c864f70e2f2f443437609ff7c1085100e
Deerlet-7 <liyan000611@outlook.com>
2020-02-26 23:21:45 +0800

    练习

--- winter2020/src/Test.java
+++ winter2020/src/Test.java
@@ -1,23 +1,34 @@
 public class Test {
 	//赎金信 一开始题目理解有问题
     public static void main(String[] args) {
-        String ransomNote = "bg";
-        String magazine = "efjbdfbbbjagbddfgdiaigdadhcfcj";
+        String ransomNote = "bbg";
+        String magazine = "efjdfjagddfgdiaigdadhcfcbj";
         System.out.println(Test.canConstruct(ransomNote,magazine));
     }
     public static boolean canConstruct(String ransomNote, String magazine) {
-            StringBuilder stringBuilder = new StringBuilder(magazine);
-            int index;
-            for (char c : ransomNote.toCharArray()) {
-                index = stringBuilder.indexOf(String.valueOf(c));
-                if (index >= 0) {
-                    stringBuilder.deleteCharAt(index);
-                } else {
-                    return false;
-                }
+        /*if (magazine.length() < ransomNote.length()) return false;
+        int caps[] = new int[26];
+        for (char c : ransomNote.toCharArray()) {
+            int index = magazine.indexOf(c, caps[c - 'a']);
+            if (index == -1)
+                return false;
+            caps[c - 97] = index + 1;
+        }
+        return true;*/
+        StringBuilder stringBuilder = new StringBuilder(magazine);
+        int index;
+        for (char c : ransomNote.toCharArray()) {
+            index = stringBuilder.indexOf(String.valueOf(c));
+            if (index >= 0) {
+                stringBuilder.deleteCharAt(index);
+            } else {
+                return false;
             }
-            return true;
-      /*  char[] ch = ransomNote.toCharArray();
+        }
+        return true;
+
+         //问题代码
+       /* char[] ch = ransomNote.toCharArray();
         char[] ch2 = magazine.toCharArray();
         int k = 0;
 
