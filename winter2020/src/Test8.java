1bac400e69c78842114bba7a1cf870e6b4161bb1
Deerlet-7 <liyan000611@outlook.com>
2020-03-07 19:54:13 +0800

    练习

---
+++ winter2020/src/Test8.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test8 {
+    public static void main(String[] args) {
+        List<Integer> L = new ArrayList<>();
+        L.add(2);
+        L.add(3);
+        L.add(4);
+        System.out.println(L);
+    }
+}
