37fbdefe0156cb3b41c2c185577f77e29df727c8
Deerlet-7 <liyan000611@outlook.com>
2020-04-01 00:11:47 +0800

    每日一题

---
+++ dailyEx/daily-studay/src/day02/Main.java
@@ -0,0 +1,25 @@
+package day02;
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(canGet(n));
+    }
+    public static int canGet(int n){
+        int count = 0;
+        if(n<1||n>100){
+            return 0;
+        }
+        while(n>2){
+            count += n/3;
+            n/=3;
+        }
+        if(n==2){
+            count++;
+        }
+        return count;
+    }
+}
+
