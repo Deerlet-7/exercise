bb9471bcc01faa12688899af36e8394632f80fbc
Deerlet-7 <liyan000611@outlook.com>
2020-04-05 21:42:24 +0800

    每日一题

--- dailyEx/daily-studay/src/day04/Test.java
+++ dailyEx/daily-studay/src/day04/Test.java
@@ -5,7 +5,7 @@ package day04;
  * 删除公共字符
  */
 import java.util.*;
-public class Test{
+/*public class Test{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String s1 = sc.nextLine();
@@ -22,5 +22,20 @@ public class Test{
             }
         }
         return sb.toString();
+    }
+}*/
+
+public class Test{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            char[] ch = sc.nextLine().toCharArray();
+            String str = sc.nextLine();
+            for(int i=0;i<ch.length;i++){
+                if(!str.contains((String.valueOf(ch[i])))){
+                    System.out.print(ch[i]);
+                }
+            }
+        }
     }
 }
