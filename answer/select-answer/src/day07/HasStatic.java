2cb909e5b7e97c25d60693e3e3429881f0024e97
Deerlet-7 <liyan000611@outlook.com>
2020-04-09 20:37:23 +0800

    选择

---
+++ answer/select-answer/src/day07/HasStatic.java
@@ -0,0 +1,18 @@
+package day07;
+
+/**
+ * Created by Deerlet-7 on 2020/4/9 16:16
+ */
+class HasStatic{
+    private static int x = 100;
+    public static void main(String args[ ]){
+        HasStatic hs1 = new HasStatic();
+        hs1.x++;
+        HasStatic hs2 = new HasStatic();
+        hs2.x++;
+        hs1=new HasStatic();
+        hs1.x++;
+        HasStatic.x--;
+        System.out.println( "x=" +x);
+    }
+}
