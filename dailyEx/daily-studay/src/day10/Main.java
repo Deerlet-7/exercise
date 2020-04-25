986becf6cb5ae32e277bf5213f2d60fea35c8fbf
Deerlet-7 <liyan000611@outlook.com>
2020-04-25 23:08:48 +0800

    最少几步变为斐波那契数列

--- dailyEx/daily-studay/src/day10/Main.java
+++ dailyEx/daily-studay/src/day10/Main.java
@@ -4,5 +4,23 @@ package day10;
  * Created by Deerlet-7 on 2020/4/10 14:56
  * fibonacci数列：给一个数，求最少要几步使其成为fibonacci数
  */
+/*import java.util.*;
 public class Main {
-}
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in);
+        int n = 0;
+        int a = 0,b = 1,c = 1;
+        if (scanner.hasNextInt()) {
+            n = scanner.nextInt();
+        }
+        while (c < n) {
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        int dis1 = n - b;
+        int dis2 = c - n;
+        System.out.println(dis1<dis2?dis1:dis2);
+    }
+}*/
+
