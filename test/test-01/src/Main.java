f10e997f71758467c77c4527c2978242d03ffce6
Deerlet-7 <liyan000611@outlook.com>
2020-06-29 21:31:39 +0800

    测试

---
+++ test/test-01/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Deerlet-7 on 2020/6/24 10:14
+ */
+public class Main {
+    public static void main(String[] args) {
+        int ch = 0x0011;
+        System.out.printf("%d",ch);
+    }
+}
+
