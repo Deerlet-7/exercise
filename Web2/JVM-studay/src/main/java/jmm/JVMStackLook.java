7a0ae7e3b19823207490bb32b6fb0965ffe482bc
Deerlet-7 <liyan000611@outlook.com>
2020-08-08 22:16:49 +0800

    web2

--- Web2/JVM-studay/src/main/java/jmm/JVMStackLook.java
+++
@@ -1,8 +0,0 @@
-package jmm;
-
-/**
- * Created by Deerlet-7 on 2020/7/30 21:34
- */
-public class JVMStackLook {
-    
-}
---
+++ dailyEx/daily-studay/src/day32/Test.java
@@ -0,0 +1,23 @@
+package day32;
+
+import java.util.Scanner;
+
+/**
+ * Created by Deerlet-7 on 2020/8/6 11:23
+ * 2的个数
+ * nowcoder.com/questionTerminal/31a9495eb02844fb8c0e9ab101053f53
+ */
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            int n = sc.nextInt();
+            System.out.println(countNumberOf2s(n));
+        }
+    }
+    public static int countNumberOf2s(int n) {
+        int count = 0;
+        
+        return count;
+    }
+}
