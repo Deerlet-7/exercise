104f6be25bfe9380fd19cf9cdd6f50eeb275d25a
Deerlet-7 <liyan000611@outlook.com>
2020-08-28 00:31:47 +0800

    优先级队列

---
+++ priorityqueue/src/TestPriorityQueue.java
@@ -0,0 +1,11 @@
+import java.util.PriorityQueue;
+
+/**
+ * Created by Deerlet-7 on 2020/8/26 15:27
+ */
+public class TestPriorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> p1 = new PriorityQueue<>();
+
+    }
+}
